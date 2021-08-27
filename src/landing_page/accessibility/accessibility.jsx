f3ad50c994aa8a40c92aa070405a43f2162df542
Digvijay <digvijay30786@gmail.com>
2021-08-27 15:51:57 +0530

    complete Landing page

---
+++ src/landing_page/accessibility/accessibility.jsx
@@ -0,0 +1,58 @@
+import './accessibility.css';
+export default function Accessibility()
+{
+    return <div className="accessibility">
+        <div className="impAccess">
+        <h4>IMPORTANT ACCESSIBILITY FEATURES THROUGHOUT OUR WEBSITE AND HOTEL.</h4>
+        <p className="pGoal">Our goal is to provide hospitality to all people who travel to Cape Cod Area. This means everyone. Our mission is to make it as easy as possible for all people including those with hearing, vision, or any other disability to learn about our property and book their stay if they so desire.</p>
+        <p className="nextGoal">If you have difficulty learning about or booking our hotel, we invite you to contact us via telephone or email and our friendly staff will gladly assist you.</p>
+        </div>
+        <div className="address">
+            <p><b>Tel: 508-665-5601</b></p>
+            <p> <b>Email: info@hyannisplazahotel.com</b></p>
+            <p><b>Address: 662 Main Street, Hyannis, 02601 </b></p>
+            <p className="braekTag">Our hotel website allows visitors to search for accessibility featured guest rooms with ease and details our property’s accessibility features on our Hotel Overview page which can be found here:</p>
+            <ul>
+            <li>Our accessible room comes with non smoking single king bed</li>
+
+            <li>Accessible Level and Ramp Entrance Into The room with wheelchair.</li>
+
+            <li>32 Inch Wide Openings to Common Areas</li>
+
+            <li>Accessible room is on ground floors and can be accessed from parking lot with a wheelchair.</li>
+
+            <li>Bathroom  doorways to and within Accessible Guest Rooms are Wheelchair Accessible</li>
+
+            <li>Breakfast Area on Ground Floor</li>
+
+            <li>Ground Floor Hotel Entrance</li>
+
+            <li>On-Site Designated Accessible Parking</li>
+
+            <li>People in Need of Accessible Features Can Book Accessible Rooms During the Same Time and Terms That Others Can Reserve Rooms</li>
+
+            <li>Service Animals Allowed</li>
+
+            <li>Smoke detectors with Strobe Lights </li>
+
+            <li>Closed Captioning Television</li>
+
+            <li>Accessible Path of Travel Around Beds</li>
+
+            <li>Shelving in bathroom is lowered can be accessed from wheelchair.</li>
+
+            <li>Hand – held Shower Wand in bath tub.</li>
+
+            <li>Grab Bar In Shower tub</li>
+
+            <li>Toilet with Grab Bar</li>
+            </ul>
+            <p className="braekTag">For additional information Please call our hotel at 508-665-5601.</p>
+            <p>Our hotel has the appropriate policies, best practices, and procedures in place to ensure that individuals with disabilities can make reservations for accessible guest rooms during the same hours and in the same manner as individuals who do not need accessible rooms.</p>
+
+            <p>If you require any assistance with booking your reservation, please contact us directly via email, phone, or social media, which you will find on our Contact Us page.</p>
+        
+            <p>If you have any questions or would like to report any issues related to the accessibility features of our hotel’s website, please contact us at info@hyannisplazahotel.com</p>
+        </div>
+      </div>
+}
---
+++ src/landing_page/policies/policies.jsx
@@ -0,0 +1,23 @@
+import './policies.css';
+export default function Policies() {
+    return <div className="policies">
+        <p>HOTEL POLICIES</p>
+        <div>
+            <ul>
+               <li>Check in time is 3:00 PM and Check-Out time is 11:00 AM.</li> 
+                <li>Valid ID is required upon Check-In.</li>
+                <li><b>Must be 21 years or older to rent a room</b></li>
+                <li>In case of any problem with regards to the room, kindly inform the Front Desk within 15 minutes of Check-In. If the staff fails to repair the issue a full refund will be granted and the guest will have to check out. </li>
+                <li>No refunds will be acknowledged to OVERNIGHT STAYS. </li>
+                <li>In case the Guest have forgotten any of their VALUABLE ITEMS in the room the hotel will preserve them for 15 days after which they will be discarded. </li>
+                <li>Hotel charges $25.00 PET FEE PER NIGHT PER ROOM. The pet has to be on leash at all times and cannot be left in the room unattended. </li>
+                <li>SERVICE PETS are exempted from the pet fee with a certified medical practitioner’s note of proof. </li>
+                 <li>Failure of not notifying or declaring that you have a pet at the front desk will lead to a $150.00 fine. </li>
+                <li>A $150.00 deposit in either cash or card will be authorized upon Check-In which will be fully refunded upon check out. In case of any kind of damage to the property or room, smoking, or any kind of misbehavior disturbing the privacy of other guests, Management reserves the right to keep the deposit in full amount. </li>
+                <li>The credit card authorization will be released upon Check-Out. Depending on your financial institution it will take 5-7 business days to show up back on the credit card. </li>
+            </ul>
+        </div>
+        <p>THANK YOU FOR YOUR PATIENCE AND UNDERSTANDING. </p>
+        <p>-MANAGEMENT</p>
+    </div>
+}
